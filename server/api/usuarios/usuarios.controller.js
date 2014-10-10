4f821ab4046693bee6aa1ba3c3ff2fedb0b68112
J. Salvador Acosta <js.acostam@gmail.com>
2014-10-10 18:20:39 -0500

    pass para salvador en el BD

--- server/api/usuarios/usuarios.controller.js
+++ server/api/usuarios/usuarios.controller.js
@@ -15,7 +15,7 @@ exports.incert = function(req, res) {
   var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : 'nancy'
+    password : 'admin'
   });
 
   connection.connect(function(err) {
