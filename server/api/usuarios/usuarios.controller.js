b9aa7beabd22309505b84c4dfb48127881016447
J. Salvador Acosta <js.acostam@gmail.com>
2014-10-10 20:33:02 -0500

    Usuarios Travis Ci v0.2

--- server/api/usuarios/usuarios.controller.js
+++ server/api/usuarios/usuarios.controller.js
@@ -5,11 +5,11 @@ var bcrypt = require('bcrypt');
 // Get list of usuarioss, unused
 exports.index = function(req, res) {
 
-  var mysql      = require('mysql');
+  var mysql = require('mysql');
   var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'admin'
+    host: 'localhost',
+    user: 'root',
+    password: 'admin'
   });
 
   connection.connect(function(err) {
@@ -17,75 +17,77 @@ exports.index = function(req, res) {
       console.error('error connecting: ' + err.stack);
       return;
     }
-      //console.log('connected as id ' + connection.threadId);
+    //console.log('connected as id ' + connection.threadId);
   });
 
   connection.query("use ss");
 
-  var query = connection.query('SELECT * FROM usuario', function(err, result){
-    if(err) {
-      throw err; debug
-      return res.send(409);
-      connection.end();
-    }else{
-      res.json(result);
-      //console.log( 'success' );
-      connection.end();
+  var query = connection.query('SELECT * FROM usuario', function(err, result) {
+      if (err) {
+        throw err;
+        debug
+        return res.send(409);
+        connection.end();
+      } else {
+        res.json(result);
+        //console.log( 'success' );
+        connection.end();
+      }
     }
-  }
 
-  // res.json([]);
+    // res.json([]);
 
-};
-
+  };
 
 
-exports.incert = function(req, res) {
 
-  var input = JSON.parse(JSON.stringify(req.body));
+  exports.incert = function(req, res) {
 
-  var mysql      = require('mysql');
-  var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'admin'
-  });
+    var input = JSON.parse(JSON.stringify(req.body));
 
-  connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-    console.log('connected as id ' + connection.threadId);
-  });
-
-  connection.query("use ss");
-  console.log(input);
-  bcrypt.hash(input.password, 8, function(err, hash) {
-    console.log(hash);
-    var data = {
-      nombre    : input.nombre,
-      correo    : input.correo,
-      hash     : hash,
-      telefono     : input.telefono,
-      institucion :  input.institucion
-    };
-    var query = connection.query('INSERT INTO usuario SET ?', data, function(err, result) {
-    console.log(query);
-    if(err) {
-      throw err; debug
-      return res.send(409);
-      connection.end();
-    }else{
-      res.json([{
-        msj : 'Registro exitoso, ahora puedes entrar al sistema',
-      }]);
-      console.log( 'success' );
-      connection.end();
-    }
+    var mysql = require('mysql');
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'admin'
     });
-    console.log(query.sql); // debug
+
+    connection.connect(function(err) {
+      if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+      }
+      console.log('connected as id ' + connection.threadId);
     });
-     
-    
-};
+
+    connection.query("use ss");
+    console.log(input);
+    bcrypt.hash(input.password, 8, function(err, hash) {
+      console.log(hash);
+      var data = {
+        nombre: input.nombre,
+        correo: input.correo,
+        hash: hash,
+        telefono: input.telefono,
+        institucion: input.institucion
+      };
+      var query = connection.query('INSERT INTO usuario SET ?', data, function(err, result) {
+        console.log(query);
+        if (err) {
+          throw err;
+          debug
+          return res.send(409);
+          connection.end();
+        } else {
+          res.json([{
+            msj: 'Registro exitoso, ahora puedes entrar al sistema',
+          }]);
+          console.log('success');
+          connection.end();
+        }
+      });
+      console.log(query.sql); // debug
+    });
+
+
+  };
