9b411308d456a553f858bdc626496b38481d9a23
J. Salvador Acosta <js.acostam@gmail.com>
2015-04-04 12:10:59 -0600

    Autor y fecha en consulta de proyecto

--- client/app/consultaProyecto/consultaProyecto.controller.js
+++ client/app/consultaProyecto/consultaProyecto.controller.js
@@ -1,9 +1,10 @@
 'use strict';
 
 angular.module('app.controllers')
-  .controller('ConsultaProyectoCtrl', function ($scope,$http,toaster, listaproyectos) {
+  .controller('ConsultaProyectoCtrl', function ($scope,$http,toaster, listaproyectos,usuariosFactory) {
   	$scope.listaproyectos = listaproyectos.data;
   	$scope.consultando = false;
+
     console.log(toaster);
     $scope.registro = false;
     $scope.toaster = {
@@ -11,10 +12,22 @@ angular.module('app.controllers')
         title: 'Titulo',
         text: 'Message'
     };
+    var monthNames = [
+        "Enero", "Febrero", "Marzo",
+        "Abril", "Mayo", "Junio", "Julio",
+        "Agosto", "Septiembre", "Octubre",
+        "Noviermbre", "Diciembre"
+    ];
+
     
     $scope.consulta = function(proyecto) {
-    	$scope.proyectoFocus = proyecto;
-    	$scope.consultando = true;
+    	usuariosFactory.getNameById(proyecto.pro_autor).then(function(response) {
+	       var d = new Date(proyecto.pro_fecha);
+	    	$scope.fecha = d.getDay()+' de '+monthNames[d.getMonth()]+', '+d.getFullYear();
+	    	$scope.proyectoFocus = proyecto;
+	    	$scope.consultando = true;
+	    	$scope.autor = response.data[0].per_nombre;
+	    });
     }
 
   });
