6b66b6c2738fe3243323e1dc3c988dd92880efd0
J. Salvador <js.acostam@gmail.com>
2015-04-09 21:23:28 -0500

    Comment y fix a lo de evaluar funciones

--- client/app/representaciones/representaciones.controller.js
+++ client/app/representaciones/representaciones.controller.js
@@ -9,10 +9,11 @@ angular.module('app.controllers')
       var valoresfuncion = [funciones[i].fun_val1,funciones[i].fun_val2,funciones[i].fun_val3,funciones[i].fun_val4,funciones[i].fun_val4,funciones[i].fun_val6,funciones[i].fun_val7,funciones[i].fun_val8,funciones[i].fun_val9]
       listafuncionesArray[i] = valoresfuncion;
     };
+    //Buscar primero la funcion asociada a la variable, si no existe entonces haver un arreglo de null
     console.log(listafuncionesArray);
     var valoresGraficar=[];
     for (var i = 0; i < unidades.length; i++) {
-      var valor = [[1,listafuncionesArray[0][unidades[i].un_s1m1v1-1]],[2,listafuncionesArray[0][unidades[i].un_s1m1v2-1]],[3,listafuncionesArray[0][unidades[i].un_s1m1v3-1]],[4,listafuncionesArray[0][unidades[i].un_s1m2v1-1]],[5,listafuncionesArray[0][unidades[i].un_s1m2v2-1]],[6,listafuncionesArray[0][unidades[i].un_s1m2v3-1]],[7,listafuncionesArray[0][unidades[i].un_s1m3v1-1]],[8,listafuncionesArray[0][unidades[i].un_s1m3v2-1]],[9,listafuncionesArray[0][unidades[i].un_s1m3v3-1]]];
+      var valor = [[1,listafuncionesArray[0][unidades[i].un_s1m1v1-1]],[2,listafuncionesArray[1][unidades[i].un_s1m1v2-1]],[3,listafuncionesArray[2][unidades[i].un_s1m1v3-1]],[4,listafuncionesArray[0][unidades[i].un_s1m2v1-1]],[5,listafuncionesArray[1][unidades[i].un_s1m2v2-1]],[6,listafuncionesArray[2][unidades[i].un_s1m2v3-1]],[7,listafuncionesArray[0][unidades[i].un_s1m3v1-1]],[8,listafuncionesArray[1][unidades[i].un_s1m3v2-1]],[9,listafuncionesArray[2][unidades[i].un_s1m3v3-1]]];
       
     };
     console.log(valor);
