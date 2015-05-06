b51df63743eca40bbad48b2ae9b372724b1c4105
NancyMejia <nancyarlette.03@gmail.com>
2015-05-06 12:34:45 -0500

    consulta modulo controller

--- client/app/consultaModulo/consultaModulo.controller.js
+++ client/app/consultaModulo/consultaModulo.controller.js
@@ -3,7 +3,6 @@
 angular.module('app.controllers')
   .controller('ConsultaModuloCtrl', function ($scope,$http,toaster, listamodulos,$state,$sce) {
  	$scope.listamodulos = listamodulos.data;
- 	/*
  	$scope.consultando = false;
 
 
@@ -30,10 +29,11 @@ angular.module('app.controllers')
         $scope.mod_nombre = modulo.mod_nombre;
     }
 
+
     $scope.variables = function(modulo){
     	console.log("consultar modulos" ,modulo);
     	
-    	$state.go('app.consultaVariable',{idmodulo:.mod_id})   
+    //	$state.go('app.consultaVariable',{idmodulo:.mod_id})   
     }
-    */
+   
  });
